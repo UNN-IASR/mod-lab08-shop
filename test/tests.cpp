a1c891c2d0537e3f883bc77a91fbff70bc282aab
Nikolate1969 <100879984+Nikolate1969@users.noreply.github.com>
2022-05-21 15:33:02 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,29 @@
-// Copyright 2021 GHA Test Team
+// Copyright 2022 GHA Test Team
 #include <gtest/gtest.h>
-#include "task1.h"
-#include "task2.h"
-#include "task3.h"
+#include "../include/task.h"
+
+
+TEST(task1, test1) {
+	Shop shop(2, 15, 100, 5, 2);
+	shop.Work();
+	int result = shop.servedClients;
+	EXPECT_EQ(15, result);
+}
+
+TEST(task2, test2) {
+	Shop shop(5, 60, 400, 3, 6);
+	shop.Work();
+	int result = shop.rejectedClients;
+	EXPECT_EQ(0, result);
+}
+
+TEST(task3, test3) {
+	Shop shop(0, 60, 400, 3, 6);
+	shop.Work();
+	int result = 0;
+	if (shop.servedClients > 0)
+		result = 1;
+  else 
+    return 0;
+	EXPECT_EQ(0, result);
+}
