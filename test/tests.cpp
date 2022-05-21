1c60fbf1884ba46a6642ba8597c8863669f06585
Nikolate1969 <100879984+Nikolate1969@users.noreply.github.com>
2022-05-21 15:46:25 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,24 +4,24 @@
 
 
 TEST(task1, test1) {
-	Shop shop(2, 15, 100, 5, 2);
+	Shop shop(1, 10, 100, 1, 1);
 	shop.Work();
 	int result = shop.servedClients;
-	EXPECT_EQ(15, result);
+	EXPECT_EQ(10, result);
 }
 
 TEST(task2, test2) {
-	Shop shop(5, 60, 400, 3, 6);
+	Shop shop(8, 92, 300, 5, 8);
 	shop.Work();
 	int result = shop.rejectedClients;
 	EXPECT_EQ(0, result);
 }
 
 TEST(task3, test3) {
-	Shop shop(0, 60, 400, 3, 6);
+	Shop shop(1, 60, 228, 5, 2);
 	shop.Work();
 	int result = 0;
-	if (shop.servedClients = 0)
+	if (shop.rejectedClients > 30)
 		result = 1;
 	EXPECT_EQ(1, result);
 }
