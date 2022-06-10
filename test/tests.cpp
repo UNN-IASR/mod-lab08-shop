5f1defb388577759c7b98b19ad8c8ca5ed21ee85
kseniyaa3 <98958076+kseniyaa3@users.noreply.github.com>
2022-06-10 18:13:26 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,3 +1,39 @@
 // Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
 #include "../include/task.h"
+
+TEST(TEST1, 1) {
+    int cassNum = 5;
+    int maxNumClients = 120;
+    int intensClients = 15;
+    int servSpeed = 30;
+    int averItemNum = 10;
+    int maxLen = 5;
+    Market m(cassNum, maxNumClients, intensClients, servSpeed, averItemNum, maxLen);
+    m.start();
+    ASSERT_GT(m.getAverageWaitingTimeForClient(), 300);
+}
+
+TEST(TEST2, 2) {
+    int cassNum = 5;
+    int maxNumClients = 100;
+    int intensClients = 5;
+    int servSpeed = 20;
+    int averItemNum = 10;
+    int maxLen = 2;
+    Market m(cassNum, maxNumClients, intensClients, servSpeed, averItemNum, maxLen);
+    m.start();
+    ASSERT_GT(m.getAmountOfUnservedClients(), 95);
+}
+
+TEST(TEST3, 3) {
+    int cassNum = 5;
+    int maxNumClients = 120;
+    int intensClients = 15;
+    int servSpeed = 30;
+    int averItemNum = 10;
+    int maxLen = 5;
+    Market m(cassNum, maxNumClients, intensClients, servSpeed, averItemNum, maxLen);
+    m.start();
+    ASSERT_GT(m.getAmountOfServedClients(), 15);
+}
