0a535f8015a7d929269eb4a541bd42a37b524a58
ActiWait <trosio2003@gmail.com>
2024-05-01 21:01:18 +0300

    start of work

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,10 @@
 // Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
-#include "task1.h"
+/* #include "task1.h"
 #include "task2.h"
-#include "task3.h"
+#include "task3.h" */
+
+TEST(sample_test_case, sample_test)
+{
+    EXPECT_EQ(1, -1);
+}
