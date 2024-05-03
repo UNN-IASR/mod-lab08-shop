e48cbde3f6b1d8433f363deb91b6f38123ba7baa
Arseniy <33318310+StatsenkoArs@users.noreply.github.com>
2024-05-03 17:07:14 +0300

    trying to make tests work

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,8 @@
 // Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
-#include "task1.h"
-#include "task2.h"
-#include "task3.h"
+#include "task.h"
+
+TEST(test_case_name, test_name)
+{
+	ASSERT_EQ(1, 0) << "1 is not equal 0";
+}
