e5b3086b002141e5f5a04fea565a114cee78bf52
YurasKamaz <tyertyn@gmail.com>
2024-05-08 13:40:10 +0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include "../include/task.h"
+
+int Fact(int n) {
+    if(n == 0)
+        return 1;
+    else
+        return n * Fact(n - 1);
+}
+
+int main() {
+    int cashDeskCount = 3; 
+    double customerIntensity = 5;
+    int productProcessingSpeed = 500;
+    int avgProductCount = 3;
+    int maxQueueLength = 5;
+    int countCustomer = 20;
+
+    double mu = 1000.0 / productProcessingSpeed / avgProductCount;
+    double ro = customerIntensity / mu;
+
+    double P0 = 0;
+    for(int i = 0; i <= cashDeskCount; i++) P0 += pow(ro, i) / Fact(i);
+    for(int i = 1; i <= maxQueueLength; i++) P0 += pow(ro, cashDeskCount + i) / (pow(cashDeskCount, i) * Fact(cashDeskCount));
+    P0 = 1/P0;
+    double Pq = P0 * (pow(ro, cashDeskCount) / Fact(cashDeskCount)) * ((1 - pow(ro/cashDeskCount, maxQueueLength)) / (1 - ro/cashDeskCount));
+    double Prej = P0 * pow(ro, cashDeskCount+maxQueueLength) / (pow(cashDeskCount, maxQueueLength) * Fact(cashDeskCount));
+    double Q = 1 - Prej;
+    double A = customerIntensity * Q;
+
+    std::cout << "\nReject prob: " << Prej
+            << "\nRel throughput: " << Q
+            << "\nAbs throughput: " << A
+            << "\nSystem load: " << ro
+            << "\nSystem idle prob: " << P0
+            << "\nQueue prob: " << Pq
+            << std::endl << std::endl;
+
+    Store store(cashDeskCount, customerIntensity, productProcessingSpeed, avgProductCount, maxQueueLength, countCustomer);
+    store.simulate();
+
+    std::cout << "Accepted customer: " << store.acceptedCustomer
+            << "\nRejected customer: " << store.rejectedCustomer 
+            << "\nReject prob: " << (double)store.rejectedCustomer / countCustomer
+            << "\nRel throughput: " << (double)store.acceptedCustomer / countCustomer
+            << "\nAbs throughput: " << (double)store.acceptedCustomer / countCustomer * customerIntensity
+            << "\nAvg queue length: " << store.sumQueueLength / countCustomer
+            << "\nAvg busy cashier time: " << store.busyCashierTime / cashDeskCount / 1000 << " sec"
+            << "\nAvg passive cashiere time: " << (store.storeTime - store.busyCashierTime / cashDeskCount) / 1000 << " sec"
+            << "\nAvg time in queue: " << (double)store.timeInQueue / store.acceptedCustomer / 1000 << " sec"
+            << std::endl;
+
+    return 0;
+}
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,27 @@
 // Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
-#include "task1.h"
-#include "task2.h"
-#include "task3.h"
+#include "../include/task.h"
+
+TEST(test1, nobodyRejected) {
+    Store s { 2, 2, 100, 5, 2, 15 };
+    s.simulate();
+    EXPECT_EQ(s.rejectedCustomer, 0);
+}
+
+TEST(test2, nobodyLost) {
+    Store s { 2, 2, 100, 5, 2, 15 };
+    s.simulate();
+    EXPECT_EQ(s.rejectedCustomer + s.acceptedCustomer, 15);
+}
+
+TEST(test3, rejectProbability) {
+    Store s { 3, 5, 500, 3, 5, 20 };
+    s.simulate();
+    ASSERT_TRUE((double)s.rejectedCustomer / 20 < 0.5);
+}
+
+TEST(test4, noQueue) {
+    Store s { 3, 5, 500, 3, 0, 20 };
+    s.simulate();
+    EXPECT_EQ(s.acceptedCustomer, 0);
+}
