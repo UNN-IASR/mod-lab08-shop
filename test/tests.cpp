5237781fcc22ea9830d6d44f64419dbd608a11a2
kateandrianova <147188563+kateandrianova@users.noreply.github.com>
2024-05-11 00:05:32 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,55 +1,54 @@
-
 #include <gtest/gtest.h>
 #include "../include/task.h"
 
 
 TEST(TestCaseName, Test1) {
-    Shop shop(5, 20, 0.1, 10, 5);
-    shop.Work();
-    int result = shop.servedСustomers + shop.unservedCustomers;
+    Store store(5, 20, 0.1, 10, 5);
+    store.work();
+    int result = store.served + store.unserved;
     EXPECT_EQ(result, 20);
 }
 
 TEST(TestCaseName, Test2) {
-    Shop shop(5, 20, 0.1, 10, 5);
-    shop.Work();
-    EXPECT_EQ(shop.servedСustomers, 12);
+    Store store(5, 20, 0.1, 10, 5);
+    store.work();
+    EXPECT_EQ(store.served, 12);
 }
 
 TEST(TestCaseName, Test3) {
-    Shop shop1(5, 20, 0.1, 10, 5);
-    Shop shop2(5, 20, 0.3, 10, 5);
-    shop1.Work();
-    shop2.Work();
+    Store store1(5, 20, 0.1, 10, 5);
+    Store store2(5, 20, 0.3, 10, 5);
+    store1.work();
+    store2.work();
     bool better = false;
-    if (shop1.servedСustomers < shop2.servedСustomers) better = true;
+    if (store1.served < store2.served) better = true;
     EXPECT_EQ(better, true);
 }
 TEST(TestCaseName, Test4) {
-    Shop shop1(5, 20, 0.1, 10, 5);
-    Shop shop2(5, 20, 0.3, 10, 5);
-    shop1.Work();
-    shop2.Work();
+    Store store1(5, 20, 0.1, 10, 5);
+    Store store2(5, 20, 0.3, 10, 5);
+    store1.work();
+    store2.work();
     bool better = true;
-    if (shop1.servedСustomers > shop2.servedСustomers) better = false;
+    if (store1.served > store2.served) better = false;
     EXPECT_EQ(better, true);
 }
 
 TEST(TestCaseName, Test5) {
-    Shop shop1(5, 20, 0.1, 10, 5);
-    Shop shop2(10, 20, 0.1, 10, 5);
-    shop1.Work();   
-    shop2.Work();
+    Store store1(5, 20, 0.1, 10, 5);
+    Store store2(10, 20, 0.1, 10, 5);
+    store1.work();   
+    store2.work();
     bool better = false;
-    if (shop1.lenQueue > shop2.lenQueue) better = true;
+    if (store1.length > store2.length) better = true;
     EXPECT_EQ(better, false);
 }
 
 TEST(TestCaseName, Test6) {
-    Shop shop1(2, 30, 0.1, 5, 10);
-    shop1.Work();
-    shop1.Statistics();
+    Store store(2, 30, 0.1, 5, 10);
+    store.work();
+    store.stat();
     bool t = false;
-    if (shop1.pPn <= 1) t = true;
+    if (store.cancel <= 1) t = true;
     EXPECT_EQ(t, true);
 }
