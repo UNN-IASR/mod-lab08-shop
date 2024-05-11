bf5da20adebab66e7f3d0924cef159c98170204b
Dekim-49 <74243140+Dekim-49@users.noreply.github.com>
2024-05-11 16:36:53 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,27 @@
-// Copyright 2021 GHA Test Team
+// Copyright 2021 Chernyadeva Anna
 #include <gtest/gtest.h>
-#include "task1.h"
-#include "task2.h"
-#include "task3.h"
+#include "task.h"
+
+TEST(Test, PropusknayaSposobnost) {
+    Shop shop(5, 1, 0.1, 10, 5);
+    shop.Start();
+    EXPECT_EQ(50, shop.servedCustomers + shop.unservedCustomers);
+}
+
+TEST(Test, ObsluzivanieNaCasse) {
+    Shop shop(1, 1, 1, 1, 1);
+    shop.Start();
+    EXPECT_EQ(50, shop.servedCustomers);
+    EXPECT_EQ(0, shop.unservedCustomers);
+}
+
+TEST(Test, VremyaRaboti) {
+    Shop shop(1, 1, 1, 1, 1);
+    auto start = std::chrono::steady_clock::now();
+    shop.Start();
+    auto stop = std::chrono::steady_clock::now();
+    int milliseconds = (stop - start).count();
+    bool f = false;
+    if (milliseconds < 6000) f = true;
+    EXPECT_EQ(f, true);
+}
