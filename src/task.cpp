13e189a681e21aa4031b098579cd853013129e4d
Анненко Миша 3821Б1ПИпр1 <146870367+AnnenkoMisha3821B1PIpr1@users.noreply.github.com>
2024-05-13 00:27:25 +0300

    Update task.cpp

--- src/task.cpp
+++ src/task.cpp
@@ -188,9 +188,10 @@ double Factorial_Recursive(int n) {
 }
 
 double Degree(double num, int degree) {
-	if (degree == 0) return 1;
-	else if (degree > 0) return num * Degree(num, degree - 1);
-	else if (degree < 0) return 1 / Degree(num, -degree);
+	//if (degree == 0) return 1;
+	//else if (degree > 0) return num * Degree(num, degree - 1);
+	//else if (degree < 0) return 1 / Degree(num, -degree);
+	return std::pow(num, degree);
 }
 
 double Calculate_Average(std::vector<double> array) {
