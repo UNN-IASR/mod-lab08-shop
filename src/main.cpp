91b6574ed496fe7f0588bdee9ec8f454152b0629
Анненко Миша 3821Б1ПИпр1 <146870367+AnnenkoMisha3821B1PIpr1@users.noreply.github.com>
2024-05-12 23:56:28 +0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,171 @@
+﻿//Copyright Annenko Misha 2024
+#include "task.h"
+
+
+void Help_main() {
+    const int count_mall = 5;
+    const int lambda = 100;
+    const int Service_time = 100;
+    const int average_products = 4;
+    const int large_queue_length = 5;
+
+    int pass_shoppers = 0;
+    int miss_shoppers = 0;
+    double average_queue_length = 0;
+    double average_overall_time = 0;
+    double average_mall_time = 0;
+    double average_wait_time = 0;
+
+    double Prej = 0;
+    double Q = 0;
+    double A = 0;
+
+    Shopping_mall array[count_mall];
+    for (int i = 0; i < count_mall; i++) {
+        array[i] = Shopping_mall(Service_time);
+        array[i].generation();
+    }
+
+    Shopper shoppers0[100];
+    std::vector<double> queues, times_at_mall;
+    for (int j = 0; j < 100; j++) {
+        shoppers0[j] = Shopper(average_products, j);
+        int _num_ = Cash_Choose(array, count_mall, large_queue_length);
+        if (_num_ != -1) {
+            array[_num_].Push_back_shopper(shoppers0[j]);
+            std::cout << "Клиент " << j + 1 <<
+                " встал в очередь кассы " << _num_ + 1 << std::endl;
+            pass_shoppers++; //
+        }
+        else {
+            miss_shoppers++; //
+            std::cout << "Клиент " << j + 1 <<
+                " не смог встать в очередь и ушел " << std::endl;
+        }
+
+        std::vector<double> avg_iter;
+        for (int i = 0; i < count_mall; i++) {
+            avg_iter.push_back(array[i].shoppers.size());
+        }
+        queues.push_back(Calculate_Average(avg_iter));
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000 / lambda));
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+
+    for (int i = 0; i < count_mall; i++) {
+        array[i].End();
+        times_at_mall.push_back(Calculate_Average(array[i].duration_work_shopper()));
+    }
+
+    std::vector<double> work_time, wait_time;
+    for (int i = 0; i < count_mall; i++) {  //avg of *cash_count* casses
+        work_time.push_back(Calculate_Average(array[i].working_time));
+        wait_time.push_back(Calculate_Average(array[i].Return_waiting_time()));
+    }
+
+    average_queue_length = Calculate_Average(queues);
+    average_overall_time = Calculate_Average(times_at_mall);
+    average_mall_time = Calculate_Average(work_time);
+    average_wait_time = Calculate_Average(wait_time);
+
+    Prej = (double)miss_shoppers / (miss_shoppers + pass_shoppers);
+    Q = 1 - Prej;
+    A = Q * lambda;
+
+    std::cout << "\n\nЭкспериментально вычисленные данные: " << std::endl;
+    std::cout << "  Число обслуженных клиентов: " <<
+        pass_shoppers << std::endl;
+    std::cout << "  Число необслуженных клиентов: " <<
+        miss_shoppers << std::endl;
+    std::cout << "  Средняя длина очереди: " << average_queue_length
+        << std::endl;
+    std::cout << "  Среднее время в очереди + на кассе: " <<
+        average_overall_time << std::endl;
+    std::cout << "  Среднее время работы кассы: " <<
+        average_mall_time << std::endl;
+    std::cout << "  Среднее время простоя кассы: " <<
+        average_wait_time << std::endl;
+    std::cout << "  Вероятность отказа: " << Prej << std::endl;
+    std::cout << "  Относительная пропускная способность: "
+        << Q << std::endl;
+    std::cout << "  Абсолютная пропускная способность: "
+        << A << std::endl;
+    std::cout << std::endl;
+
+    double myu = 1000 / Service_time;
+    double ro = lambda / myu;
+    double sum = 0;
+    for (int i = 0; i <= count_mall + large_queue_length; i++) {
+        if (i <= count_mall) {
+            sum += Degree(ro, i) / Factorial_Recursive(i);
+        }
+        else {
+            sum += Degree(ro, i) / (Factorial_Recursive(count_mall) *
+                Degree(count_mall, i - count_mall));
+        }
+    }
+    double P0 = (double)1 / sum;
+    Prej = P0 * Degree(ro, count_mall + large_queue_length) /
+        (Degree(count_mall, large_queue_length) * Factorial_Recursive(count_mall));
+    Q = 1 - Prej;
+    A = lambda * Q;
+
+    std::cout << "Теоретически вычисленные данные: " << std::endl;
+    std::cout << "  Интенсивность потока клиентов: " << lambda << std::endl;
+    std::cout << "  Интенсивность обслуживания: " << myu << std::endl;
+    std::cout << "  Интенсивность нагрузки канала: " << ro << std::endl;
+    std::cout << "  Вероятность отказа: " << Prej << std::endl;
+    std::cout << "  Относительная пропускная способность: " << Q
+        << std::endl;
+    std::cout << "  Абсолютная пропускная способность: " << A << std::endl;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    Help_main();
+
+    //В VS не работает google test
+    //ТЕСТ1
+    Shopper shopper = Shopper(5, 1);
+    int count = shopper.amount_of_products;
+    if (count == 5) {
+        std::cout << "\n\nЧётко 1";
+    }
+    //ТЕСТ2
+    Shopping_mall mall = Shopping_mall(20);
+    bool check = mall.in_service;
+    if (check == false) {
+        std::cout << "\nЧётко 2";
+    }
+    //ТЕСТ3
+    Shopping_mall mall1 = Shopping_mall(20);
+    Shopper shopper1 = Shopper(4, 2);
+    Shopper shopper2 = Shopper(3, 1);
+    mall1.Push_back_shopper(shopper1);
+    mall1.Push_back_shopper(shopper2);
+    int count1 = mall1.shoppers.size();
+    if (count1 == 2) {
+        std::cout << "\nЧётко 3";
+    }
+    //ТЕСТ4
+    Shopping_mall mall2 = Shopping_mall(20);
+    int number = Cash_Choose(&mall2, 2, 4);
+    if (number == 0) {
+        std::cout << "\nЧётко 4";
+    }
+    //ТЕСТ5
+    Shopping_mall mall3 = Shopping_mall(20);
+    double k = 3;
+    while (k < 12) {
+        mall3.Push_back_working_time((double)k);
+        k++;
+    }
+    std::vector<double> times = mall3.working_time;
+    if (times[4] == (double)7) {
+        std::cout << "\nЧётко 5";
+    }
+}
---
+++ src/task.cpp
@@ -0,0 +1,200 @@
+//Copyright Annenko Misha 2024
+#include "task.h"
+
+//Покупатель
+Shopper::Shopper(int amount, int numb) {
+	amount_of_products = amount;
+	shopper_number = numb;
+    transferred_to_cash = false;
+}
+
+Shopper::Shopper() {
+	shopper_number = 0;
+	amount_of_products = 0;
+	transferred_to_cash = false;
+}
+
+//Торговый центр или как по заданию супермаркет
+Shopping_mall::Shopping_mall(int ServiceTime) {
+	in_service = false;
+	service_time = ServiceTime;
+}
+
+Shopping_mall::Shopping_mall() {
+	in_service = false;
+	service_time = 0;
+}
+
+void Shopping_mall::Push_back_working_time(double work) {
+	working_time.push_back(work);
+}
+
+void Shopping_mall::Push_back_waiting_time(double wait) {
+	waiting_time.push_back(wait);
+}
+
+void Shopping_mall::start_of_service() {
+	in_service = true;
+}
+
+bool Shopping_mall::Check_empty_shoppers() {
+	if (shoppers.empty()) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+void Shopping_mall::Pop_front_shoppers() {
+	if (Check_empty_shoppers() == true) {
+		throw std::runtime_error("Вектор клиентов пуст");
+	} else if (Check_empty_shoppers() == false) {
+		shoppers.erase(shoppers.begin());
+	}
+}
+
+void Shopping_mall::Push_back_shopper(Shopper shopper) {
+	shoppers.push_back(shopper);
+	time_spent_on_shoppers.push_back({ std::chrono::high_resolution_clock::now(),
+	std::chrono::high_resolution_clock::now() });
+}
+
+bool Shopping_mall::Check_empty_waiting_time() {
+	if (waiting_time.empty()) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+std::vector<double> Shopping_mall::Return_waiting_time() {
+	if (Check_empty_waiting_time() == true) {
+		return std::vector<double>{0};
+	} else {
+		return waiting_time;
+	}
+}
+
+void Shopping_mall::End() {
+	end = true;
+}
+
+void Shopping_mall::shopper_add_times
+(std::chrono::high_resolution_clock::time_point point2, int number) {
+	time_spent_on_shoppers[number] = { time_spent_on_shoppers[number][0], point2 };
+}
+
+void Shopping_mall::generation() {
+	Service = new std::thread([this] {servicing(*this); });
+}
+
+std::vector<double> Shopping_mall::duration_work_shopper() {
+	std::vector<double> duration_work;
+	int i = 0;
+	while( i < time_spent_on_shoppers.size()) {
+		duration_work.push_back((double)std::chrono::duration_cast
+			<std::chrono::milliseconds>
+			(time_spent_on_shoppers[i][1] -
+				time_spent_on_shoppers[i][0]).count());
+		i++;
+	}
+	return duration_work;
+}
+
+void servicing(Shopping_mall& mall) {
+
+	Shopper shopper;
+	int shopper_service = 0;
+
+	std::chrono::time_point start_waiting =
+		std::chrono::high_resolution_clock::now();
+	std::chrono::time_point end_waiting =
+		std::chrono::high_resolution_clock::now();
+	bool launch_control = false;
+
+	while (!mall.end) {
+
+		if (mall.shoppers.size() != 0) {
+
+			if (launch_control) {
+				launch_control = false;
+				end_waiting = std::chrono::high_resolution_clock::now();
+
+				auto duration0 = std::chrono::duration_cast<std::chrono::milliseconds>(end_waiting - start_waiting);
+
+				double buf0 = (double)duration0.count();
+				mall.Push_back_waiting_time(buf0);
+			}
+
+			shopper = mall.shoppers.at(0);
+
+			auto start = std::chrono::high_resolution_clock::now();
+
+			mall.Pop_front_shoppers();
+			mall.start_of_service();//begin servicing
+			for (int i = 1; i <= shopper.amount_of_products; i++) {
+
+				std::this_thread::sleep_for(std::chrono::milliseconds
+				(mall.service_time));
+			}
+
+			mall.shopper_add_times
+			(std::chrono::high_resolution_clock::now(),
+				shopper_service);
+			shopper_service++;
+
+			auto stop = std::chrono::high_resolution_clock::now();
+			auto shopper = std::chrono::duration_cast
+				<std::chrono::milliseconds>(stop - start);
+			double buf1 = (double)shopper.count();
+			mall.Push_back_working_time(buf1);
+		} else {
+			if (launch_control) end_waiting = std::chrono::high_resolution_clock::now();
+			else {
+				start_waiting = std::chrono::high_resolution_clock::now();
+				launch_control = true;
+			}
+		}
+	}
+}
+
+int Cash_Choose(Shopping_mall array[], int money_count, int large_queue) {
+	int num = -1;
+	int k = 0;
+	while (k < money_count) {
+		if (!(array[k].in_service) &&
+			!(array[k].shoppers.size() > 0) && num == -1) num = k;
+		k++;
+	}
+	if (num == -1) {
+		int queue = large_queue;
+		int i = 0;
+		while (i < money_count) {
+			if (array[i].shoppers.size() < queue) {
+				num = i; queue = array[i].shoppers.size();
+			}
+			i++;
+		}
+	}
+	return num;
+}
+
+//Взял функцию у себя из 7 лаборторной работы
+double Factorial_Recursive(int n) {
+	//Рекурсивный вызов, при i != 0
+	if (n != 0) return n * Factorial_Recursive(n - 1);
+	//Базовый случай: факториал от 0 равен 1
+	else return 1;
+}
+
+double Calculate_Average(std::vector<double> array) {
+	double sum = std::accumulate(array.begin(), array.end(), 0.0);
+	double count = std::distance(array.begin(), array.end());
+	return sum / count;
+}
+
+double Degree(double num, int degree) {
+	if (degree == 0) return 1;
+	else if (degree > 0) return num * Degree(num, degree - 1);
+	else if (degree < 0) return 1 / Degree(num, -degree);
+}
