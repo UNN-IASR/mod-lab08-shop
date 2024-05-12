7d4128d28b582ded65d939018bc774643628859b
Eltazh <profeltzero@mail.ru>
2024-05-12 22:43:00 +0300

    fix

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,23 +4,23 @@
 TEST(Test1, NoRejected) {
     Market m(2, 100, 2, 5, 2);
     m.Work(15);
-    EXPECT_EQ(m.statRejected, 0);
+    EXPECT_EQ(m.rejectedCustomer, 0);
 }
 
 TEST(Test2, NoLost) {
     Market m(2, 100, 2, 5, 2);
     m.Work(15);
-    EXPECT_EQ(m.statAccepted + m.statRejected, 15);
+    EXPECT_EQ(m.acceptedCustomer + m.rejectedCustomer, 15);
 }
 
 TEST(Test3, RejectProbability) {
     Market m(3, 500, 5, 3, 5);
     m.Work(20);
-    ASSERT_TRUE(m.statRejected / 20 < 0.5);
+    ASSERT_TRUE(m.rejectedCustomer / 20 < 0.5);
 }
 
 TEST(Test4, NoQueue) {
     Market m(3, 500, 5, 3, 0);
     m.Work(20);
-    EXPECT_EQ(m.statAccepted, 0);
+    EXPECT_EQ(m.acceptedCustomer, 0);
 }
