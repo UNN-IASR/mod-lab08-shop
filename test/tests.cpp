05cdf6b090288db17d0db3de7258f4f12714b29f
Arseniy <33318310+StatsenkoArs@users.noreply.github.com>
2024-05-13 20:54:58 +0300

    name fix

--- test/tests.cpp
+++ test/tests.cpp
@@ -45,7 +45,7 @@ TEST (test_Shopper_lifetime, Shopper_life_clock) {
 
 }
 
-TEST (tets_Shop_all_served, Shop_100) {
+TEST (test_Shop_all_served, Shop_100) {
 	int cash = 10; //количество касс (каналов обслуживания)
     double shoppersIntensity = 6; //интенсивность потока покупателей (заявки на обслуживание)
     double productCashTime = 0.5; //скорость обработки товара на кассе
@@ -59,7 +59,7 @@ TEST (tets_Shop_all_served, Shop_100) {
 	ASSERT_EQ(customers, sh.statistic.served);
 }
 
-TEST (tets_Shop_none_served, Shop_0) {
+TEST (test_Shop_none_served, Shop_0) {
 	int cash = 0; //количество касс (каналов обслуживания)
     double shoppersIntensity = 6; //интенсивность потока покупателей (заявки на обслуживание)
     double productCashTime = 0.5; //скорость обработки товара на кассе
