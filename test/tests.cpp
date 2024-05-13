fcb3059384e02299d889887e33ebfac10304d950
kateandrianova <147188563+kateandrianova@users.noreply.github.com>
2024-05-13 11:48:46 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,52 +3,55 @@
 
 
 TEST(TestCaseName, Test1) {
-    Store store(5, 20, 0.1, 10, 5);
+    Store store(10, 30, 0.2, 8, 6);
     store.work();
-    int result = store.served + store.unserved;
-    EXPECT_EQ(result, 20);
+    EXPECT_EQ(store.served + store.unserved, 20);
 }
 
 TEST(TestCaseName, Test2) {
-    Store store(5, 20, 0.1, 10, 5);
+    Store store(10, 30, 0.2, 8, 6);
     store.work();
     EXPECT_EQ(store.served, 12);
 }
 
 TEST(TestCaseName, Test3) {
-    Store store1(5, 20, 0.1, 10, 5);
-    Store store2(5, 20, 0.3, 10, 5);
+    Store store1(10, 30, 0.2, 8, 6);
+    Store store2(10, 30, 0.4, 8, 6);
     store1.work();
     store2.work();
-    bool better = false;
-    if (store1.served < store2.served) better = true;
-    EXPECT_EQ(better, true);
+    bool f = false;
+    if (store1.served < store2.served) 
+        f = true;
+    EXPECT_EQ(f, true);
 }
 TEST(TestCaseName, Test4) {
-    Store store1(5, 20, 0.1, 10, 5);
-    Store store2(5, 20, 0.3, 10, 5);
+    Store store1(10, 30, 0.2, 8, 6);
+    Store store2(10, 30, 0.4, 8, 6);
     store1.work();
     store2.work();
-    bool better = true;
-    if (store1.served > store2.served) better = false;
-    EXPECT_EQ(better, true);
+    bool f = true;
+    if (store1.served > store2.served) 
+        f = false;
+    EXPECT_EQ(f, true);
 }
 
 TEST(TestCaseName, Test5) {
-    Store store1(5, 20, 0.1, 10, 5);
-    Store store2(10, 20, 0.1, 10, 5);
+    Store store1(10, 30, 0.2, 8, 6);
+    Store store2(10, 30, 0.4, 8, 6);
     store1.work();   
     store2.work();
-    bool better = false;
-    if (store1.length > store2.length) better = true;
-    EXPECT_EQ(better, false);
+    bool f = false;
+    if (store1.avera > store2.avera) 
+        f = true;
+    EXPECT_EQ(f, false);
 }
 
 TEST(TestCaseName, Test6) {
-    Store store(2, 30, 0.1, 5, 10);
+    Store store(10, 30, 0.2, 8, 6);
     store.work();
     store.stat();
-    bool t = false;
-    if (store.cancel <= 1) t = true;
-    EXPECT_EQ(t, true);
+    bool f = false;
+    if (store.cancel < 1) 
+        f = true;
+    EXPECT_EQ(f, true);
 }
