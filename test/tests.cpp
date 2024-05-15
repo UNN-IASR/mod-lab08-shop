38bb59e6d381601a268e29587f10fc78b106ff5f
marikozlova <162039344+marikozlova@users.noreply.github.com>
2024-05-15 11:37:54 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,35 @@
-// Copyright 2021 GHA Test Team
+// Copyright 2024 GHA Test Team
 #include <gtest/gtest.h>
-#include "task1.h"
-#include "task2.h"
-#include "task3.h"
+#include "task.h"
+
+
+TEST(test1, T1) {
+	Shop shop(2, 10, 50, 9, 5);
+	shop.Run();
+	EXPECT_TRUE(shop.Served() <= 10);
+}
+TEST(test2, T2) {
+	Shop shop(2, 10, 10, 9, 5);
+	shop.Run();
+	EXPECT_TRUE(shop.Unserved() <= 15);
+}
+TEST(test3, T3) {
+	Shop shop(1, 20, 50, 5, 1);
+	shop.Run();
+	EXPECT_TRUE(shop.Av_lenght() <= 30);
+}
+TEST(test4, T4) {
+	Shop shop(2, 10, 10, 9, 5);
+	shop.Run();
+	EXPECT_TRUE(shop.Av_time() <= 1);
+}
+TEST(test5, T5) {
+	Shop shop(2, 10, 10, 9, 5);
+	shop.Run();
+	EXPECT_TRUE(shop.Av_wait_time() <= 1);
+}
+TEST(test6, T6) {
+	Shop shop(2, 10, 10, 9, 5);
+	shop.Run();
+	EXPECT_TRUE(shop.Av_work_time() >= 1);
+}
