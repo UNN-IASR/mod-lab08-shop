2123cc774f7ac4461b11c22341dd22df20b599a4
Варочкина Галя <147079170+varochkinagalia@users.noreply.github.com>
2024-05-18 19:53:31 +0300

    tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,3 +5,11 @@ TEST(Test, Client) {
     Client client(5, 2, 3);
     EXPECT_EQ(client.time_v_ocheredi, 3);
 }
+TEST(KassaTest, TestObslugivanie) {
+    Kassa kassa;
+    Client client(5, 2, 3);
+    std::thread t1(&Kassa::obslugivanie, &kassa, &client);
+    t1.join();
+    EXPECT_TRUE(kassa.polnaya);
+    //EXPECT_EQ(kassa.getWaitTime(), client.getTimeObslugivania());
+}
