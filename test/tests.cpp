0c830998742fdc377f2e7f83a11f40e1d759d29c
Варочкина Галя <147079170+varochkinagalia@users.noreply.github.com>
2024-05-18 19:06:59 +0300

    tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,7 @@
 // Copyright 2021 GHA Test Team
 #include <gtest/gtest.h>
-#include "task1.h"
-#include "task2.h"
-#include "task3.h"
+#include "task.h"
+TEST(ClientTest, TestConstructor) {
+    Client client(5, 2, 3);
+    EXPECT_EQ(client.time_v_ocheredi, 3);
+}
