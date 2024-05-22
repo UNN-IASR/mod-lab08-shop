83704f3d7f53a6f9ba944c2a94f309b287b5a572
MichailImchikov <93055597+MichailImchikov@users.noreply.github.com>
2024-05-22 16:51:20 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,7 +3,7 @@
 #include "../include/task.h"
 TEST(TestCase, Test1) {
     Shop shop(1,1,1);
-    shop.cahs.AddNewClient(1, 1);
+    shop.AddNewClient(1, 1);
   	do{
 	} while (cahs.clientQueue.size() > 0);
     EXPECT_EQ(shop.Buy, 1);
