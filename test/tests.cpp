69cf48bc54eea12f223442f4453d954bd83043c8
Krylov Vladimir <68692106+krylofficiall@users.noreply.github.com>
2024-06-01 00:53:16 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,7 +4,7 @@
 TEST(TestCaseName, Test1) {
     Restore restore(12, 34, 0.3, 9, 7);
     restore.job();
-    EXPECT_EQ(restore.ready + restore.unready, 30);
+    EXPECT_EQ(restore.servis + restore.not_servis, 30);
 }
 
 TEST(TestCaseName, Test2) {
@@ -13,7 +13,7 @@ TEST(TestCaseName, Test2) {
     restore1.job();
     restore2.job();
     bool f = false;
-    if (restore1.ready < restore2.ready) 
+    if (restore1.servis < restore2.not_servis) 
         f = true;
     EXPECT_EQ(f,true);
 }
@@ -23,7 +23,7 @@ TEST(TestCaseName, Test3) {
     restore1.job();
     restore2.job();
     bool f = true;
-    if (restore1.ready > restore2.ready) 
+    if (restore1.servis > restore2.not_servis) 
         f = false;
     EXPECT_EQ(f, true);
 }
@@ -42,7 +42,7 @@ TEST(TestCaseName, Test4) {
 TEST(TestCaseName, Test5) {
     Restore restore(12, 34, 0.3, 9, 7);
     restore.job();
-    restore.stats();
+    restore.statisi();
     bool f = false;
     if (restore.can < 1) 
         f = true;
