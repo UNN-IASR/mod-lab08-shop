e18cf4264328ed7c304a8e1df16914c4088501a9
bartyomi <148716654+bartyomi@users.noreply.github.com>
2024-06-04 16:21:29 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,37 @@
 // Copyright 2021 GHA Test Team
-#include <gtest/gtest.h>
-#include "task1.h"
-#include "task2.h"
-#include "task3.h"
+#include "task.h"
+
+TEST(TestCaseName, Test1) {
+    Store store(5, 50, 0.1, 10, 10);
+    store.Run();
+    EXPECT_EQ(store.serviced + store.notServiced, 50);
+}
+
+TEST(TestCaseName, Test2) {
+    Store store1(5, 50, 0.1, 10, 10);
+    Store store2(5, 50, 0.1, 10, 10);
+    store1.Run();
+    store2.Run();
+    bool equaled = true;
+    if (store1.serviced != store2.serviced) 
+        equaled = false;
+    EXPECT_EQ(equaled,true);
+}
+
+TEST(TestCaseName, Test3) {
+    Store store1(5, 50, 0.1, 10, 10);
+    store1.Run();
+    EXPECT_EQ(store.notServiced, 30);
+}
+
+TEST(TestCaseName, Test4) {
+    Store store1(5, 10, 0.1, 10, 10);
+    store1.Run();
+    EXPECT_EQ(store.notServiced, 0);
+}
+
+TEST(TestCaseName, Test5) {
+    Store store1(5, 10, 0.1, 10, 10);
+    store1.Run();
+    EXPECT_EQ(store.queue.size(), 0);
+}
